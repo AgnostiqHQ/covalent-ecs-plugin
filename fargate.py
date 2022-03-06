052ffc7ea5e32ed4654f695fd70d3cb89d703b02
Will Cunningham <will@agnostiq.ai>
2022-03-06 15:10:11 -0500

    adding much more infra stuff

--- fargate.py
+++ fargate.py
@@ -20,6 +20,17 @@
 
 """AWS Fargate executor plugin for the Covalent dispatcher."""
 
+# TODO: There are several IAM policies to consider:
+#       1. The policy needed to use the FargateExecutor, without provisioning infrastructure
+#       2. Same as above, except additionally allowing provisioning
+#       3. ECS task execution role's policy
+#       4. ECS task's policy (used to grant permissions to the script running within the Docker container)
+
+# TODO: Consider adding a sync/async bool option to execute.  Sync should poll the result, while
+#       async should include a callback within the script that's run on the remote machine. This will
+#       allow the runner to interact with any given executor in both ways; we expect synchronous behavior
+#       in a self-hosted runner, and async behavior in the hosted (Covalent Cloud) runner.
+
 import base64
 import os
 import shutil
@@ -34,7 +45,20 @@ from covalent._shared_files.util_classes import DispatchInfo
 from covalent._workflow.transport import TransportableObject
 from covalent.executor import BaseExecutor
 
-# TODO: Create defaults dict
+from covalent._shared_files.logger import app_log
+
+_EXECUTOR_PLUGIN_DEFAULTS = {
+    "credentials": os.environ.get("AWS_SHARED_CREDENTIALS_FILE") or os.path.join(os.environ["HOME"], ".aws/credentials"),
+    "profile": "",
+    "s3_uri": "s3://covalent-fargate-task-resources",
+    "ecr_repo_name": "covalent-fargate-task-images",
+    "ecs_cluster_name": "covalent-fargate-cluster",
+    "ecs_task_family_name": "covalent-fargate-tasks",
+    "ecs_task_role_name": "CovalentFargateTaskRole",
+    "vcpu": "0.25",
+    "memory": "0.5",
+    "provision": False
+}
 
 executor_plugin_name = "FargateExecutor"
 
@@ -47,12 +71,16 @@ class FargateExecutor(BaseExecutor):
 
     def __init__(
         self,
-        credentials = "", #filename
-        profile: Optional[str] = None, #credential profile
-        s3_uri = "",
-        ecr_repo_name = "covalent-fargate",
-        ecs_cluster,
-        ecs_service
+        credentials: str,
+        profile: str,
+        s3_uri: str,
+        ecr_repo_name: str,
+        ecs_cluster_name: str,
+        ecs_task_family_name: str,
+        ecs_task_role_name: str,
+        vcpu: float,
+        memory: float,
+        provision: bool,
     ):
         super().__init__()
 
@@ -60,6 +88,12 @@ class FargateExecutor(BaseExecutor):
         self.profile = profile
         self.s3_uri = s3_uri
         self.ecr_repo_name = ecr_repo_name
+        self.ecs_cluster_name = ecs_cluster_name
+        self.ecs_task_family_name = ecs_task_family_name
+        self.ecs_task_role_name = ecs_task_role_name
+        self.vcpu = vcpu
+        self.memory = memory
+        self.provision = provision
 
     def execute(
         self,
@@ -77,8 +111,12 @@ class FargateExecutor(BaseExecutor):
         result_filename = f"result-{dispatch_id}-{node_id}.pkl"
         task_results_dir = os.path.join(results_dir, dispatch_id)
         docker_working_dir = "/opt/covalent"
+        image_tag = f"{dispatch_id}-{node_id}"
 
         os.environ["AWS_SHARED_CREDENTIALS_FILE"] = self.credentials
+
+        if self.provision:
+            app_log.warning("The FargateExecutor's 'provision' flag is intended for debugging purposes only, and enabling it is *not* efficient. For general usage, use the accompanying CDK script to provision and manage persistent cloud resources.")
 
         with self.get_dispatch_context(dispatch_info), tempfile.NamedTemporaryFile(dir=self.cache_dir) as f, tempfile.NamedTemporaryFile(dir=self.cache_dir) as g:
 
@@ -139,8 +177,6 @@ CMD ["{docker_working_dir}/{func_filename}"]
             local_dockerfile = os.path.join(task_results_dir, f"Dockerfile-{dispatch_id}-{node_id}")
             shutil.copyfile(g.name, local_dockerfile)
 
-            image_tag = f"{dispatch_id}-{node_id}"
-
             # Build the Docker image
             docker_client = docker.from_env()
             image, build_log = docker_client.images.build(
@@ -148,16 +184,40 @@ CMD ["{docker_working_dir}/{func_filename}"]
                 tag=image_tag
             )
 
-            # ECR Login
+            # ECR config
             ecr = boto3.client("ecr", profile_name=self.profile)
 
             ecr_username = "AWS"
             ecr_credentials = ecr.get_authorization_token()["authorizationData"][0]
             ecr_password = base64.b64decode(ecr_credentials["authorizationToken"]).replace(b"AWS:", b"").decode("utf-8")
-            ecr_url = ecr_credentials["proxyEndpoint"]
-            ecr_repo_uri = f"{ecr_url.replace('https://', '')}/{self.ecr_repo_name}:{image_tag}"
+            ecr_registry = ecr_credentials["proxyEndpoint"]
+            ecr_repo_uri = f"{ecr_registry.replace('https://', '')}/{self.ecr_repo_name}:{image_tag}"
+            account = ecr_repo_uri.split(".")[0]
 
-            docker_client.login(username=ecr_username, password=ecr_password, registry=ecr_url)
+            docker_client.login(username=ecr_username, password=ecr_password, registry=ecr_registry)
+
+            if self.provision:
+                # Check if the repo already exists
+                repo_exists = False
+                # TODO: Handle paginated response using get_paginator
+                response = ecr.describe_repositories(registryId=ecr_registry)
+                repos = response["repositories"]
+                for repo in repos:
+                    if repo["repositoryName"] == self.ecr_repo_name:
+                        repo_exists = True
+                        break
+
+                # Create the ECR repo
+                if not repo_exists:
+                    ecr.create_repository(
+                        registryId=ecr_registry,
+                        repositoryName=self.ecr_repo_name,
+                        imageTagMutability="IMMUTABLE",
+                        encryptionConfiguration={
+                            "encryptionType": "KMS",
+                        },
+                    )
+                    # TODO: Check response
 
             # Tag the image
             image.tag(ecr_repo_uri, tag=image_tag)
@@ -165,12 +225,90 @@ CMD ["{docker_working_dir}/{func_filename}"]
             # Push to ECR
             result = docker_client.images.push(ecr_repo_uri, tag=image_tag)
 
-            # Deploy on ECS
+            # ECS config
             ecs = boto3.client("ecs", profile_name=self.profile)
-            ecs.update_service(
-                cluster=self.ecs_cluster, 
-                service=self.ecs_service, 
-                forceNewDeployment=True
+
+            if self.provision:
+                # Check if the cluster already exists
+                cluster_exists = False
+                paginator = ecs.get_paginator("list_clusters")
+                page_iterator = paginator.paginate()
+                for page in page_iterator:
+                    if cluster_exists:
+                        break
+
+                    for arn in page["clusterArns"]:
+                        cluster_name = ecs.describe_clusters(clusters=[arn])["clusters"][0]["clusterName"]
+                        if cluster_name == self.ecs_cluster_name:
+                            cluster_exists = True
+                            break
+
+                #Create the ECS cluster
+                if not cluster_exists:
+                    ecs.create_cluster(clusterName=self.ecs_cluster_name)
+                    #TODO: Check response
+
+                # Check if the task role already exists
+                # TODO: Need to check the role as well as the corresponding policy
+                #       The policy will need to allow access to S3 and Braket
+                ecs_task_role_exists = False
+                iam = boto3.client("iam", profile_name=self.profile)
+                roles = iam.list_roles()["Roles"]
+                for role in roles:
+                    if roles["RoleName"] == self.ecs_task_role_name:
+                        ecs_task_role_exists = True
+                        break
+
+                # Create the role used by the container
+                iam.create_role(
+                    RoleName=self.ecs_task_role_name,
+                    #TODO: continue filling this in
+                )
+
+
+                # Check if task definition family already exists
+                task_family_exists = False
+                paginator = ecs.get_paginator("list_task_definition_families")
+                page_iterator = paginator.paginate()
+                for page in page_iterator:
+                    if page["families"] == self.ecs_task_family_name:
+                        task_family_exists = True
+                        break
+
+                # Create the task definition
+                if not task_family_exists:
+                    ecs.register_task_definition(
+                        family=self.ecs_task_family_name,
+                        taskRoleArn=self.ecs_task_role_name,
+                        executionRoleArn=f"arn:aws:iam::{account}:role/ecsTaskExecutionRole",
+                        networkMode="awsvpc",
+                        requiresCompabilities=["FARGATE"],
+                        containerDefinitions=[
+                            {
+                                "name": f"covalent-task-{image_tag}",
+                                "image": ecr_repo_uri,
+                                "essential": True,
+                            },
+                        ],
+                        cpu=str(self.cpu*1024),
+                        memory=str(self.memory*1024),
+                    )
+                    #TODO: Check response
+
+            # Run the task
+            response = ecs.run_task(
+                taskDefinition=self.ecs_task_family_name,
+                launchType="FARGATE",
+                cluster=self.ecs_cluster_name,
+                count=1,
+                networkConfiguration={
+                    "awsvpcConfiguration": {
+                        "subnets": [], #TODO: Provision a subnet
+                    },
+                },
             )
+            # TODO: Check response
+
+            # TODO: Either poll and return a result (sync) or return response status (async)
 
             return 42
