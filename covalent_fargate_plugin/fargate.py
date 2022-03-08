089cb2d1d7bde8de6a2b5ef13e35db91fb448346
Will Cunningham <will@agnostiq.ai>
2022-03-08 00:58:41 -0500

    testing

--- covalent_fargate_plugin/fargate.py
+++ covalent_fargate_plugin/fargate.py
@@ -35,10 +35,14 @@ import base64
 import os
 import shutil
 import subprocess
+import tempfile
 
 import boto3
 import docker
 
+import cloudpickle as pickle
+
+from pathlib import Path
 from typing import Any, Dict, List, Tuple
 
 from covalent._shared_files.util_classes import DispatchInfo
@@ -49,15 +53,16 @@ from covalent._shared_files.logger import app_log
 
 _EXECUTOR_PLUGIN_DEFAULTS = {
     "credentials": os.environ.get("AWS_SHARED_CREDENTIALS_FILE") or os.path.join(os.environ["HOME"], ".aws/credentials"),
-    "profile": "",
-    "s3_uri": "s3://covalent-fargate-task-resources",
+    "profile": os.environ.get("AWS_PROFILE") or "",
+    "s3_bucket_name": "covalent-fargate-task-resources",
     "ecr_repo_name": "covalent-fargate-task-images",
     "ecs_cluster_name": "covalent-fargate-cluster",
     "ecs_task_family_name": "covalent-fargate-tasks",
     "ecs_task_role_name": "CovalentFargateTaskRole",
-    "vcpu": "0.25",
-    "memory": "0.5",
-    "provision": False
+    "vcpu": 0.25,
+    "memory": 0.5,
+    "provision": False,
+    "cache_dir": "/tmp/covalent"
 }
 
 executor_plugin_name = "FargateExecutor"
@@ -73,7 +78,7 @@ class FargateExecutor(BaseExecutor):
         self,
         credentials: str,
         profile: str,
-        s3_uri: str,
+        s3_bucket_name: str,
         ecr_repo_name: str,
         ecs_cluster_name: str,
         ecs_task_family_name: str,
@@ -81,12 +86,13 @@ class FargateExecutor(BaseExecutor):
         vcpu: float,
         memory: float,
         provision: bool,
+        **kwargs,
     ):
-        super().__init__()
+        super().__init__(**kwargs)
 
         self.credentials = credentials
         self.profile = profile
-        self.s3_uri = s3_uri
+        self.s3_bucket_name = s3_bucket_name
         self.ecr_repo_name = ecr_repo_name
         self.ecs_cluster_name = ecs_cluster_name
         self.ecs_task_family_name = ecs_task_family_name
@@ -114,11 +120,26 @@ class FargateExecutor(BaseExecutor):
         image_tag = f"{dispatch_id}-{node_id}"
 
         os.environ["AWS_SHARED_CREDENTIALS_FILE"] = self.credentials
+        os.environ["AWS_PROFILE"] = self.profile
+
+        # TODO: Move this to BaseExecutor
+        Path(self.cache_dir).mkdir(parents=True, exist_ok=True)
 
         if self.provision:
             app_log.warning("The FargateExecutor's 'provision' flag is intended for debugging purposes only, and enabling it is *not* efficient. For general usage, use the accompanying CDK script to provision and manage persistent cloud resources.")
 
-        with self.get_dispatch_context(dispatch_info), tempfile.NamedTemporaryFile(dir=self.cache_dir) as f, tempfile.NamedTemporaryFile(dir=self.cache_dir) as g:
+        with self.get_dispatch_context(dispatch_info), \
+            tempfile.NamedTemporaryFile(dir=self.cache_dir) as function_file, \
+            tempfile.NamedTemporaryFile(dir=self.cache_dir, mode="w") as script_file, \
+            tempfile.NamedTemporaryFile(dir=self.cache_dir, mode="w") as dockerfile_file:
+
+            # Write serialized function to file
+            pickle.dump(function, function_file)
+            function_file.flush()
+
+            # Upload pickled function to S3
+            s3 = boto3.client("s3")
+            s3.upload_file(function_file.name, self.s3_bucket_name, func_filename)
 
             # Write execution script to file
             python_script = """
@@ -126,11 +147,11 @@ import os
 import boto3
 import cloudpickle as pickle
 
-local_func_filename = os.path.join({docker_working_dir}, {func_filename})
-local_result_filename = os.path.join({docker_working_dir}, {result_filename})
+local_func_filename = os.path.join("{docker_working_dir}", "{func_filename}")
+local_result_filename = os.path.join("{docker_working_dir}", "{result_filename}")
 
 s3 = boto3.client("s3")
-s3.download_file({s3_uri}, {func_filename}, local_func_filename)
+s3.download_file("{s3_bucket_name}", "{func_filename}", local_func_filename)
 
 with open(local_func_filename, "rb") as f:
     function = pickle.load(f).get_deserialized()
@@ -140,24 +161,25 @@ result = function(*{args}, **{kwargs})
 with open(local_result_filename, "wb") as f:
     pickle.dump(result, f)
 
-s3.upload_file(local_result_filename, {s3_uri}, {result_filename})
+s3.upload_file(local_result_filename, "{s3_bucket_name}", "{result_filename}")
 """.format(
                 func_filename=func_filename,
                 args=args,
                 kwargs=kwargs,
-                s3_uri=self.s3_uri,
+                s3_bucket_name=self.s3_bucket_name,
                 result_filename=result_filename,
                 docker_working_dir=docker_working_dir,
             )
-            f.write(python_script)
-            f.flush()
+            script_file.write(python_script)
+            script_file.flush()
 
             # Write Dockerfile
+            # TODO: Really we should be modifying the existing Dockerfile
             dockerfile = """
 FROM python:3.8-slim-buster
 
-RUN apt-get update && apt-get install -y \
-  gcc \
+RUN apt-get update && apt-get install -y \\
+  gcc \\
   && rm -rf /var/lib/apt/lists/*
 RUN pip install --no-cache-dir --use-feature=in-tree-build boto3 cloudpickle
 
@@ -166,26 +188,29 @@ COPY {func_filename} {docker_working_dir}
 WORKDIR {docker_working_dir}
 
 ENTRYPOINT [ "python" ]
-CMD ["{docker_working_dir}/{func_filename}"]
+CMD ["{docker_working_dir}/{func_basename}"]
 """.format(
-                func_filename=f.name,
+                func_filename=script_file.name,
+                func_basename=os.path.basename(script_file.name),
                 docker_working_dir=docker_working_dir,
             )
-            g.write(dockerfile)
-            g.flush()
+            dockerfile_file.write(dockerfile)
+            dockerfile_file.flush()
 
-            local_dockerfile = os.path.join(task_results_dir, f"Dockerfile-{dispatch_id}-{node_id}")
-            shutil.copyfile(g.name, local_dockerfile)
+            local_dockerfile = os.path.join(task_results_dir, f"Dockerfile_node-{node_id}")
+            shutil.copyfile(dockerfile_file.name, local_dockerfile)
 
             # Build the Docker image
             docker_client = docker.from_env()
             image, build_log = docker_client.images.build(
+                path=self.cache_dir,
                 dockerfile=local_dockerfile, 
                 tag=image_tag
             )
+            return f"{image_tag}", "", ""
 
             # ECR config
-            ecr = boto3.client("ecr", profile_name=self.profile)
+            ecr = boto3.client("ecr")
 
             ecr_username = "AWS"
             ecr_credentials = ecr.get_authorization_token()["authorizationData"][0]
