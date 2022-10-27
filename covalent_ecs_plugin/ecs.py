6378ba27e784ea3c175b1e84c86d722497ee9461
Alejandro Esquivel <ae@alejandro.ltd>
2022-10-26 18:28:01 -0700

    Stable tag update ECR URI (#46)
    * Updated tag of hardcoded ECR URI to `stable`
    
    * Updated CHANGELOG

--- covalent_ecs_plugin/ecs.py
+++ covalent_ecs_plugin/ecs.py
@@ -60,7 +60,7 @@ EXECUTOR_PLUGIN_NAME = "ECSExecutor"
 FUNC_FILENAME = "func-{dispatch_id}-{node_id}.pkl"
 RESULT_FILENAME = "result-{dispatch_id}-{node_id}.pkl"
 CONTAINER_NAME = "covalent-task-{dispatch_id}-{node_id}"
-COVALENT_EXEC_BASE_URI = "public.ecr.aws/covalent/covalent-executor-base:latest"
+COVALENT_EXEC_BASE_URI = "public.ecr.aws/covalent/covalent-executor-base:stable"
 
 
 class ECSExecutor(AWSExecutor):
