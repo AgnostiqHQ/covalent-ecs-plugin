29707f00bdb4bdb48ac02a8913db607995babea4
Alejandro Esquivel <ae@alejandro.ltd>
2022-11-02 15:00:30 -0700

    Changed functional tests to use pytest, using .env config for executor instance

--- tests/functional_tests/basic_workflow.py
+++ tests/functional_tests/basic_workflow.py
@@ -18,40 +18,34 @@
 #
 # Relief from the License may be granted by purchasing a commercial license.
 
-
-import sys
-
 import covalent as ct
+import pytest
 
-# Extract terraform outputs & instantiate executor
-import executor_instance
+from tests.functional_tests.fixtures.executor import executor
 
 # Basic Workflow
 
 
-@ct.electron(executor=executor_instance.executor)
-def join_words(a, b):
-    return ", ".join([a, b])
+@pytest.mark.functional_tests
+def test_basic_workflow():
+    @ct.electron(executor=executor)
+    def join_words(a, b):
+        return ", ".join([a, b])
 
+    @ct.electron
+    def excitement(a):
+        return f"{a}!"
 
-@ct.electron
-def excitement(a):
-    return f"{a}!"
+    @ct.lattice
+    def basic_workflow(a, b):
+        phrase = join_words(a, b)
+        return excitement(phrase)
 
+    # Dispatch the workflow
+    dispatch_id = ct.dispatch(basic_workflow)("Hello", "World")
+    result = ct.get_result(dispatch_id=dispatch_id, wait=True)
+    status = str(result.status)
 
-@ct.lattice
-def basic_workflow(a, b):
-    phrase = join_words(a, b)
-    return excitement(phrase)
+    print(result)
 
-
-# Dispatch the workflow
-dispatch_id = ct.dispatch(basic_workflow)("Hello", "World")
-result = ct.get_result(dispatch_id=dispatch_id, wait=True)
-status = str(result.status)
-
-print(result)
-
-if status == str(ct.status.FAILED):
-    print("Basic Workflow failed to run.")
-    sys.exit(1)
+    assert status == str(ct.status.COMPLETED)
